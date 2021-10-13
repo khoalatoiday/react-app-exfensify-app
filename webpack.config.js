3e105ecae4fac14b8d78d31fbafebb6a42dc614e
khoalatoiday <demonhunter942000@gmail.com>
2021-10-13 11:37:21 +0700

    set production and build

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = (env) => {
     output: {
       path: path.join(__dirname, "public"), // __dirname/public ~ output file
       filename: "bundle.js",
+      publicPath: '/public/'
     },
 
     plugins: [
